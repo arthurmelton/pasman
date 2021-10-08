6caff6c5831ec9b0833869fae1d3db361904473d
Arthur Melton <amtitan@icloud.com>
2021-10-07 22:38:49 -0400

    first step

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use directories::BaseDirs;
+use std::path::Path;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    if let Some(base_dirs) = BaseDirs::new() {
+        if !Path::new(&[base_dirs.config_dir().to_str().unwrap(), "/pas.man"].join("")).exists() {
+            File::create(&[base_dirs.config_dir().to_str().unwrap(), "/pas.man"].join(""));
+        }
+        println!("What is your password?");
+        let mut line = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        line = line.trim().to_string();
+        let password = format!("{:x}", md5::compute(matches.value_of(line).unwrap().to_string()));
+    }
 }
