e9e96ac3750507db8fd8d16451f128c62d46abae
arthurmelton <amtitan@icloud.com>
2021-10-08 15:32:55 -0400

    delete password

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use std::str::FromStr;
 use std::time::Duration;
 use humantime::format_duration;
 use std::fs;
+use prettytable::{Table, Row, Cell};
 
 fn main() {
     let matches = App::new("pasman")
@@ -62,8 +63,17 @@ fn main() {
     if matches.is_present("test") {
         let mut num = 0;
         let now = SystemTime::now();
+        let nonce = Nonce::from_slice(b"unique nonce");
+        let password = format!("{:x}", md5::compute("one"));
+        let key = Key::from_slice(password.as_ref());
+        let cipher = Aes256Gcm::new(key);
+        let ciphertext = cipher.encrypt(nonce, b"billys passwords".as_ref())
+            .expect("encryption failure!"); 
         while now.elapsed().unwrap().as_millis() < 1000 {
-            format!("{:x}", md5::compute(num.to_string()));
+            let password = format!("{:x}", md5::compute(num.to_string()));
+            let key = Key::from_slice(password.as_ref());
+            let cipher = Aes256Gcm::new(key);
+            cipher.decrypt(nonce, ciphertext.as_ref());
             num += 1;
         }
         println!("your computer can do {} hashes per second (Some computers can do WAY more than this, and your password could already be hashed so it would take no time even if it was really long, so pick a password that NO ONE has ever used)\n1 chars = {}\n2 chars = {}\n3 chars = {}\n4 chars = {}\n5 chars = {}\n6 chars = {}\n7 chars = {}\n8 chars = {}\n9 chars = {}\n10 chars = {}\n11 chars = {}\n12 chars = {}\n13 chars = {}\n14 chars = {}\n15 chars = {}\n16 chars = {}\n17 chars = {}\n18 chars = {}", num, format_duration(Duration::new((72_u128.pow(1) / num) as u64, 0)).to_string(), format_duration(Duration::new((72_u128.pow(2) / num) as u64, 0)).to_string(), format_duration(Duration::new((72_u128.pow(3) / num) as u64, 0)).to_string(), format_duration(Duration::new((72_u128.pow(4) / num) as u64, 0)).to_string(), format_duration(Duration::new((72_u128.pow(5) / num) as u64, 0)).to_string(), format_duration(Duration::new((72_u128.pow(6) / num) as u64, 0)).to_string(), format_duration(Duration::new((72_u128.pow(7) / num) as u64, 0)).to_string(), format_duration(Duration::new((72_u128.pow(8) / num) as u64, 0)).to_string(), format_duration(Duration::new((72_u128.pow(9) / num) as u64, 0)).to_string(), format_duration(Duration::new((72_u128.pow(10) / num) as u64, 0)).to_string(), format_duration(Duration::new((72_u128.pow(11) / num) as u64, 0)).to_string(), format_duration(Duration::new((72_u128.pow(12) / num) as u64, 0)).to_string(), format_duration(Duration::new((72_u128.pow(13) / num) as u64, 0)).to_string(), format_duration(Duration::new((72_u128.pow(14) / num) as u64, 0)).to_string(), format_duration(Duration::new((72_u128.pow(15) / num) as u64, 0)).to_string(), format_duration(Duration::new((72_u128.pow(16) / num) as u64, 0)).to_string(), format_duration(Duration::new((72_u128.pow(17) / num) as u64, 0)).to_string(), format_duration(Duration::new((72_u128.pow(18) / num) as u64, 0)).to_string());
@@ -86,7 +96,7 @@ fn main() {
                 line = matches.value_of("password").unwrap().trim().to_string();
             }
             else {
-                println!("What is your master password? (you can run the --test on the executable to see how long it would take to crack the password)");
+                println!("What is your master password? (you can run the --test to see how long it would take to crack the password)");
                 std::io::stdin().read_line(&mut line).unwrap();
                 line = line.trim().to_string();
             }
@@ -100,9 +110,6 @@ fn main() {
             let nonce = Nonce::from_slice(b"unique nonce");
             let mut contents = String::new();
             File::open(&[base_dirs.config_dir().to_str().unwrap(), "/pas.man"].join("")).unwrap().read_to_string(&mut contents);
-            let decoded = hex::decode(contents.clone()).unwrap();
-            cipher.decrypt(nonce, decoded.as_ref())
-                .expect("decryption failure!"); // NOTE: handle this error to avoid panics!
             if contents.len() == 0 {
                 println!("Lets add a password what do you want the name to be?");
                 let mut new_accont:String = "".to_string();
@@ -123,15 +130,27 @@ fn main() {
                     new_pass = new_pass.trim().to_string();
                 }
                 let ciphertext = cipher.encrypt(nonce, [new_accont, " : ".to_string(), new_pass].join("").as_ref())
-                    .expect("encryption failure!"); // NOTE: handle this error to avoid panics!
+                    .expect("encryption failure!"); 
                 write_file([base_dirs.config_dir().to_str().unwrap(), "/pas.man"].join(""), hex::encode(ciphertext));
             }
             else {
+                let decoded = hex::decode(contents.clone()).unwrap();
+                cipher.decrypt(nonce, decoded.as_ref())
+                    .expect("decryption failure!");
                 if matches.is_present("list") {
                     let decoded = hex::decode(contents.clone()).unwrap();
                     let plaintext = cipher.decrypt(nonce, decoded.as_ref())
-                        .expect("decryption failure!"); // NOTE: handle this error to avoid panics!
-                    println!("{}", String::from_utf8_lossy(&*plaintext).to_string());
+                        .expect("decryption failure!");
+                    let mut table = Table::new();
+                    table.add_row(Row::new(vec![
+                        Cell::new("Account"),
+                        Cell::new("Password")]));
+                    for x in String::from_utf8_lossy(&*plaintext).to_string().split("\n") {
+                        table.add_row(Row::new(vec![
+                            Cell::new(x.split(" : ").nth(0).unwrap()),
+                            Cell::new(x.split(" : ").nth(1).unwrap())]));
+                    }
+                    table.printstd();
                     std::process::exit(1);
                 }
                 if matches.is_present("create") {
@@ -155,16 +174,16 @@ fn main() {
                     }
                     let decoded = hex::decode(contents.clone()).unwrap();
                     let plaintext = cipher.decrypt(nonce, decoded.as_ref())
-                        .expect("decryption failure!"); // NOTE: handle this error to avoid panics!
+                        .expect("decryption failure!"); 
                     let ciphertext = cipher.encrypt(nonce, [String::from_utf8_lossy(&*plaintext).to_string(), "\n".to_string(), new_accont, " : ".to_string(), new_pass].join("").as_ref())
-                        .expect("encryption failure!"); // NOTE: handle this error to avoid panics!
+                        .expect("encryption failure!"); 
                     write_file([base_dirs.config_dir().to_str().unwrap(), "/pas.man"].join(""), hex::encode(ciphertext));
                     std::process::exit(1);
                 }
                 if matches.is_present("find") {
                     let decoded = hex::decode(contents.clone()).unwrap();
                     let plaintext = cipher.decrypt(nonce, decoded.as_ref())
-                        .expect("decryption failure!"); // NOTE: handle this error to avoid panics!
+                        .expect("decryption failure!"); 
                     let mut account = matches.value_of("find").unwrap().to_string();
                     if account.contains(" ") {
                         println!("Sorry the account you are looking for can not have a space in it");
@@ -203,11 +222,11 @@ fn main() {
                     }
                     std::process::exit(1);
                 }
-                println!("Type the one you want\n1: add a password\n2: find a password\n3: list all passwords (UNSECURE)\n4: Delete all passwords");
+                println!("Type the one you want\n1: add a password\n2: find a password\n3: list all passwords (UNSECURE)\n4: Delete a password\n5: Delete all passwords");
                 let mut type_pick:String = "".to_string();
                 std::io::stdin().read_line(&mut type_pick).unwrap();
                 type_pick = type_pick.trim().to_string();
-                if !["1", "2", "3"].contains(&&*type_pick) {
+                if !["1", "2", "3", "4", "5"].contains(&&*type_pick) {
                     println!("Sorry but you have to pick on of the above type 1 etc");
                     std::io::stdin().read_line(&mut type_pick).unwrap();
                     type_pick = type_pick.trim().to_string();
@@ -233,15 +252,15 @@ fn main() {
                     }
                     let decoded = hex::decode(contents.clone()).unwrap();
                     let plaintext = cipher.decrypt(nonce, decoded.as_ref())
-                        .expect("decryption failure!"); // NOTE: handle this error to avoid panics!
+                        .expect("decryption failure!"); 
                     let ciphertext = cipher.encrypt(nonce, [String::from_utf8_lossy(&*plaintext).to_string(), "\n".to_string(), new_accont, " : ".to_string(), new_pass].join("").as_ref())
-                        .expect("encryption failure!"); // NOTE: handle this error to avoid panics!
+                        .expect("encryption failure!"); 
                     write_file([base_dirs.config_dir().to_str().unwrap(), "/pas.man"].join(""), hex::encode(ciphertext));
                 }
                 else if type_pick == "2" {
                     let decoded = hex::decode(contents.clone()).unwrap();
                     let plaintext = cipher.decrypt(nonce, decoded.as_ref())
-                        .expect("decryption failure!"); // NOTE: handle this error to avoid panics!
+                        .expect("decryption failure!"); 
                     println!("what account are you looking for?");
                     loop {
                         let mut account: String = "".to_string();
@@ -287,11 +306,75 @@ fn main() {
                 else if type_pick == "3" {
                     let decoded = hex::decode(contents.clone()).unwrap();
                     let plaintext = cipher.decrypt(nonce, decoded.as_ref())
-                        .expect("decryption failure!"); // NOTE: handle this error to avoid panics!
-                    println!("{}", String::from_utf8_lossy(&*plaintext).to_string());
+                        .expect("decryption failure!");
+                    let mut table = Table::new();
+                    table.add_row(Row::new(vec![
+                        Cell::new("Account"),
+                        Cell::new("Password")]));
+                    for x in String::from_utf8_lossy(&*plaintext).to_string().split("\n") {
+                        table.add_row(Row::new(vec![
+                            Cell::new(x.split(" : ").nth(0).unwrap()),
+                            Cell::new(x.split(" : ").nth(1).unwrap())]));
+                    }
+                    table.printstd();
+                }
+                else if type_pick == "5" {
+                    fs::remove_file([base_dirs.config_dir().to_str().unwrap(), "/pas.man"].join(""));
                 }
                 else if type_pick == "4" {
-                    fs::remove_file([base_dirs.config_dir().to_str().unwrap(), "/pas.man"].join(""));
+                    let decoded = hex::decode(contents.clone()).unwrap();
+                    let plaintext = cipher.decrypt(nonce, decoded.as_ref())
+                        .expect("decryption failure!");
+                    println!("what account are you looking for?");
+                    loop {
+                        let mut account: String = "".to_string();
+                        std::io::stdin().read_line(&mut account).unwrap();
+                        account = account.trim().to_string();
+                        if account.contains(" ") {
+                            println!("Sorry the account you are looking for can not have a space in it");
+                            std::io::stdin().read_line(&mut account).unwrap();
+                            account = account.trim().to_string();
+                        }
+                        let split = String::from_utf8_lossy(&*plaintext).to_string().replace("\n", " : ");
+                        let split: Vec<&str> = split.split(" : ").collect();
+                        let mut account_names: Vec<String> = Vec::new();
+                        let mut passwords: Vec<String> = Vec::new();
+                        for x in 0..split.len() {
+                            if x % 2 == 0 {
+                                account_names.push(split[x].to_string());
+                            } else {
+                                passwords.push(split[x].to_string());
+                            }
+                        }
+                        let mut name = Vec::new();
+                        for x in account_names.clone() {
+                            if x.contains(&account) {
+                                name.push(x);
+                            }
+                        }
+                        if name.len() == 0 {
+                            println!("I have not found any passwords with that name");
+                            std::process::exit(1);
+                        } else if name.len() == 1 {
+                            for x in 0..account_names.clone().len() {
+                                if name.first().unwrap() == &account_names[x] {
+                                    let mut new_file = Vec::new();
+                                    let text = String::from_utf8_lossy(&*plaintext).to_string();
+                                    for x in text.lines() {
+                                        if x.split(" : ").nth(0).unwrap() != name.first().unwrap() {
+                                            new_file.push(x);
+                                        }
+                                    }
+                                    let ciphertext = cipher.encrypt(nonce, new_file.join("\n").as_ref())
+                                        .expect("encryption failure!");
+                                    write_file([base_dirs.config_dir().to_str().unwrap(), "/pas.man"].join(""), hex::encode(ciphertext));
+                                }
+                            }
+                            std::process::exit(1);
+                        } else {
+                            println!("I have found\n{}\nWhat password do you want?", name.join("\n"));
+                        }
+                    }
                 }
             }
         }
